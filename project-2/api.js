2f3186c4e9476e71ddc5723015fd55fc54c1570a
Federico Gratti <federicogratti@gmail.com>
2021-07-10 14:19:13 +0200

    Bug fixed

--- project-2/api.js
+++ project-2/api.js
@@ -3,8 +3,9 @@ import { Math as TMath } from 'three';
 export default class Api {
     test() {
         let clamped_number = TMath.clamp(2, 0, 1);
-
+        console.log(clamped_number)
         let result = Math.max(clamped_number, 3);
+        console.log(result)
     }
 }
 
